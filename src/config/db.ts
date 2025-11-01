8d3a30b6414f8c2e14b442d7c70d886397b8750a
Renanluizssx9 <renanluizssx@gmail.com>
2025-11-01 09:56:16 -0300

    added features

--- src/config/db.ts
+++ src/config/db.ts
@@ -3,8 +3,8 @@ import mongoose from "mongoose";
 export async function connectDB() {
   try {
     await mongoose.connect(process.env.MONGO_URI!);
-    console.log("✅ Conectado ao MongoDB Atlas");
+    console.log("✅ Connected to MongoDB Atlas");
   } catch (err) {
-    console.error("❌ Erro ao conectar ao MongoDB:", err);
+    console.error("❌ Error connecting to MongoDB:", err);
   }
 }
