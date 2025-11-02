75bf99edb85eac82558082a7da8a5f29fbae3d9c
Renanluizssx9 <renanluizssx@gmail.com>
2025-11-02 07:30:04 -0300

    added features

---
+++ eslint.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+  env: {
+    node: true,
+    es2021: true,
+  },
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  rules: {
+    "no-console": "off",
+    "@typescript-eslint/no-unused-vars": "warn",
+  },
+};
