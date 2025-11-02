4f4c04d48b59ff831545570466f314c521d381be
Renanluizssx9 <renanluizssx@gmail.com>
2025-11-02 07:33:59 -0300

    added features

--- eslint.config.js
+++ eslint.config.js
@@ -1,17 +1,29 @@
-module.exports = {
-  parser: "@typescript-eslint/parser",
-  plugins: ["@typescript-eslint"],
-  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
-  env: {
-    node: true,
-    es2021: true,
+import plugin from "@typescript-eslint/eslint-plugin";
+import parser from "@typescript-eslint/parser";
+import globals from "globals";
+
+export default [
+  {
+    ignores: ["node_modules", "dist", ".next", "build"],
   },
-  parserOptions: {
-    ecmaVersion: "latest",
-    sourceType: "module",
+  {
+    files: ["**/*.{ts,tsx,js}"],
+    languageOptions: {
+      parser,
+      parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+      },
+      globals: {
+        ...globals.node,
+      },
+    },
+    plugins: {
+      "@typescript-eslint": plugin,
+    },
+    rules: {
+      "no-console": "off",
+      "@typescript-eslint/no-unused-vars": "warn",
+    },
   },
-  rules: {
-    "no-console": "off",
-    "@typescript-eslint/no-unused-vars": "warn",
-  },
-};
+];
